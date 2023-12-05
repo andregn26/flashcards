dda7b9404ebcb3c1d15481a2966c2eaff4ecbe6a
andregn26 <andregn26@gmail.com>
2023-12-05 10:25:39 +0000

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import NewTopicForm from "../components/NewTopicForm";
+import NewQuizForm from "../components/NewQuizForm";
+import Topics from "../features/topics/Topics";
+import Topic from "../features/topics/Topic";
+import Card from "../features/cards/Card";
+import Quiz from "../features/quizzes/Quiz";
+import Quizzes from "../features/quizzes/Quizzes";
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<AppLayout />}>
+						<Route path="topics" element={<Topics />} />
+						<Route path="topics/new" element={<NewTopicForm />} />
+						<Route path="topics/:topicId" element={<Topic />} />
+						<Route path="quizzes" element={<Quizzes />} />
+						<Route path="quizzes/new" element={<NewQuizForm />} />
+						<Route path="quizzes/:quizId" element={<Quiz />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
